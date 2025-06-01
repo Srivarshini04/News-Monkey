efdd7e22a8fefb1e0a4af43507f73b2b8489dfa1
Srivarshini04 <komirishettysrivarshini@gmail.com>
2025-06-01 11:54:39 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import {Link} from "react-router-dom";
+import {Link,NavLink} from "react-router-dom";
 
 const Navbar=()=> {
     return (
@@ -13,14 +13,14 @@ const Navbar=()=> {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/">Home</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/general">General</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/business">Business</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/entertainment">Entertainment</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/health">Health</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/science">Science</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/sports">Sports</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/technology">Technology</NavLink></li>
         </ul>
         </div>
     </div>
